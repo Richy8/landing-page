ed02a78a99017c5415aa7221f317f41479d82383
Efemena Elvis <menaelvisjones@gmail.com>
2021-11-30 07:47:59 +0100

    Added toast snack bar

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,22 @@
       <router-view />
     </transition>
 
+    <!-- SNACK TOAST BAR  -->
+    <toast-snack-bar
+      v-if="show_alert"
+      :message="alert.message"
+      :type="alert.type"
+      @closeAlert="toggleAlert"
+    />
+
     <!-- GRADELY PORTAL TARGET -->
     <portal-target name="gradely-modals"> </portal-target>
   </div>
 </template>
 
 <script>
+import toastSnackBar from "@/shared/components/global-comps/toast-snack-bar";
+
 export default {
   name: "App",
 
@@ -19,6 +29,31 @@ export default {
     htmlAttrs: {
       lang: "en",
       amp: true,
+    },
+  },
+
+  components: {
+    toastSnackBar,
+  },
+
+  data: () => ({
+    alert: {
+      message: "Alert message here...",
+      type: "success",
+    },
+
+    show_alert: false,
+  }),
+
+  created() {
+    // RESPONSE ALERT EVENT
+    this.$bus.$on("show_response_alert", (data) => this.toggleAlert(data));
+  },
+
+  methods: {
+    toggleAlert(data = {}) {
+      Object.keys(data).length ? (this.alert = data) : null;
+      this.show_alert = !this.show_alert;
     },
   },
 };
